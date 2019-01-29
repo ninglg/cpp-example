67294695faba34e8906352af31957e2d4526754a
ninglg <ninglg@gmail.com>
2019-01-30 00:37:08 +0800

    update

--- str.cpp
+++ str.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <vector>
 #include <string>
 #include "str.h"
 
-int main() {
-    std::string s(2, 'a');
-    std::cout << s << std::endl;
-    std::cout << num << std::endl;
+using namespace std;
+
+int main()
+{
+    // vector + string
+    vector<string> str;
+    str.push_back("abc");
+    str.push_back("xyz");
+
+    vector<string>::iterator iter;
+
+    for (iter = str.begin(); iter != str.end(); iter++) {
+        cout << *iter << endl;
+    }
+
+    // const
+    cout << num << endl;
+
+    // typedef
+    tip c = 8;
+    cout << c << endl;
+
+    // enum
+    Color d = Green;
+    cout << d << endl;
+
     return 0;
 }
--- str.h
+++ str.h
@@ -1,6 +1,14 @@
 #ifndef _STR_H
 #define _STR_H
 
-const int num = 1;
+typedef int tip;
+
+const int num = 10;
+
+enum Color {
+    Red,
+    Yello,
+    Green
+};
 
 #endif
